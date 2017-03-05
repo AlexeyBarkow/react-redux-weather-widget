55fd534b2adc3d61e07f59957e6965691999d8a9
AlexeyBarkow <imbarkowal@gmail.com>
2017-03-05 22:06:43 +0300

    fixed tooltip offset

--- app/containers/TooltipDomain.js
+++ app/containers/TooltipDomain.js
@@ -1,31 +1,33 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import classnames from 'classnames/dedupe';
 
-const renderTooltip = ({ top, left, right, bottom, tooltipText }, type, index) => {
-    if (type === 'bottom') {
-        return (
-            <div key={`${type}-${index}`} style={{ left, top: bottom }} className={classnames('tooltip in', type)}>
-                <div className="tooltip-inner">
-                    {tooltipText}
+class TooltipDomain extends Component {
+    renderTooltip = ({ top, left, right, bottom, tooltipText }, type, index) => {
+        if (type === 'bottom') {
+            return (
+                <div key={`${type}-${index}`} style={{ left: (left + right) / 2, top: bottom }} className={classnames('tooltip in', type)}>
+                    <div className="tooltip-inner">
+                        {tooltipText}
+                    </div>
+                    <div className="tooltip-arrow" />
                 </div>
-                <div className="tooltip-arrow" />
+            );
+        }
+        return undefined;
+    };
+
+    render() {
+        const { children, className, tooltip } = this.props;
+        const tooltips = Object.entries(tooltip)
+            .map(([type, data], index) => data && this.renderTooltip(data, type, index));
+
+        return (
+            <div className={classnames(className, 'tooltip-container')}>
+                { children }
+                { tooltips }
             </div>
         );
     }
-    return undefined;
-};
-
-
-function TooltipDomain({ children, className, tooltip }) {
-    const tooltips = Object.entries(tooltip)
-        .map(([type, data], index) => data && renderTooltip(data, type, index));
-
-    return (
-        <div className={classnames(className, 'tooltip-container')}>
-            { children }
-            { tooltips }
-        </div>
-    );
 }
 
 TooltipDomain.propTypes = {
