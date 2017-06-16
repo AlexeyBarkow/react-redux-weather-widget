f1ddf842982e69938c49a34e0a81ffde40d1139e
Aliaksei Barkou <aliaksei_barkou@epam.com>
2017-06-16 15:44:08 +0300

    Add a Procfile

--- app/history.prod.js
+++ app/history.prod.js
@@ -1,9 +1,13 @@
-import { createHistory } from 'history';
-import { useRouterHistory } from 'react-router';
-import { BASEPATH_URL } from './utils/constants';
-
-const browserHistory = useRouterHistory(createHistory)({
-    basename: BASEPATH_URL,
-});
+// import { createHistory } from 'history';
+// import { useRouterHistory } from 'react-router';
+// import { BASEPATH_URL } from './utils/constants';
+//
+// const browserHistory = useRouterHistory(createHistory)({
+//     basename: BASEPATH_URL,
+// });
+//
+// export default browserHistory;
+import { browserHistory } from 'react-router';
 
 export default browserHistory;
+
--- server/constants.js
+++ server/constants.js
@@ -1,5 +1,5 @@
 
-export const PORT = 3005;
+export const PORT = process.env.PORT || 3005;
 export const DOMAIN = 'http://localhost';
 export const GOOGLE_CLIENT_ID = '338319760653-dc9jtb5kd4qqfmecg1svn45o1d3envvb.apps.googleusercontent.com';
 export const GOOGLE_SECRET = 'narazJsmrTwvnOHOaq1Nl5SQ';
